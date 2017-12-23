bc2773c0313f62d2e0c36f1e38db599374d604aa
jhyun94 <hyunjonathan@gmail.com>
2017-12-23 12:44:28 -0600

    assignment 1

---
+++ main.vue
@@ -0,0 +1,14 @@
+new Vue({
+	el: "#root",
+	data: {
+		name: 'Jon',
+		age: 23,
+		imgSrc: 'https://vuejs.org/images/logo.png'
+	},
+
+	methods: {
+		randomNum(){
+			return Math.random() * 1
+		}
+	}
+})
