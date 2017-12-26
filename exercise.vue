0a3d8e066e4a0694d76601a26fcd11643ab1a02c
jhyun94 <hyunjonathan@gmail.com>
2017-12-26 15:04:00 -0600

    styling

--- exercise.vue
+++ exercise.vue
@@ -1,24 +1,39 @@
 new Vue({
-	el: '#exercise',
-	data: {
-		value: 0
-	},
-	computed: {
-		result(){
-			if(this.value !== 37){
-				return 'not there yet'
-			}
-
-			return 'done'
-		}
-	},
-	watch: {
-		result: function(){
-			var local = this;
-			console.log(local);
-			setTimeout( function(){
-				local.value=0
-			}, 5000)
-		}
-	}
-})
+  el: '#exercise',
+  data: {
+  effectClasses: {
+    highlight: false,
+      shrink: true
+    },
+  float: 'float',
+    userClass: '',
+    isVisible: true,
+    myStyle: {
+    width: '100px',
+      height: '150px',
+      backgroundColor: 'gray'
+    },
+    progressBar: {
+    width: '0px',
+      backgroundColor: 'red'
+    }
+  },
+  methods: {
+    startEffect: function() {
+    var vm = this;
+      setInterval(function() {
+      vm.effectClasses.highlight = !vm.effectClasses.highlight;
+        vm.effectClasses.shrink = !vm.effectClasses.shrink;
+      }, 1000);
+    },
+    startProgress: function() {
+    var vm = this;
+      var width = 0;
+     
+    setInterval(function() {
+      width = width + 10;
+      vm.progressBar.width = width + 'px';
+      }, 500);
+    }
+  }
+});
