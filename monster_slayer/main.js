d055d9159d14d85fe1b8fee4ecbfc7c72c8f0387
jhyun94 <hyunjonathan@gmail.com>
2017-12-27 13:34:23 -0600

    methods for attack,special,heal

--- monster_slayer/main.js
+++ monster_slayer/main.js
@@ -4,5 +4,25 @@ new Vue({
 		playerLife: 100,
 		monsterLife: 100,
 		start: false
+	},
+	methods: {
+		attack(){
+			this.playerLife = this.playerLife - this.generateAttack();
+			this.monsterLife = this.monsterLife - this.generateAttack();
+		},
+		specialAttack(){
+			this.playerLife = this.playerLife - this.generateAttack();
+			this.monsterLife = this.monsterLife - this.generateSpecial();
+		},
+		heal(){
+			this.playerLife = this.playerLife + this.generateAttack();
+			this.playerLife = this.playerLife - this.generateAttack();
+		}
+		generateGeneral(){
+			return Math.floor(Math.random() * 11);
+		},
+		generateSpecial(){
+			return Math.floor(Math.random() * 21);
+		}
 	}
 })
