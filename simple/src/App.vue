790fb9644b950e4bcae28c748c717d38549c6588
jhyun94 <hyunjonathan@gmail.com>
2018-01-05 15:34:28 -0600

    removed old components

--- simple/src/App.vue
+++ simple/src/App.vue
@@ -1,30 +1,9 @@
 <template>
-    <div class="container">
-        <app-header></app-header>
-        <hr>
-        <div class="row">
-            <servers></servers>
-            <app-server-details></app-server-details>
-        </div>
-        <hr>
-        <app-footer></app-footer>
-    </div>
+   
 </template>
 
 <script>
-    import Header from './components/Shared/header.vue';
-    import Footer from './components/Shared/footer.vue';
-    import Servers from './components/Server/servers.vue';
-    import ServerDetails from './components/server/serverDetails.vue';
-
-    export default {
-        components: {
-            appHeader: Header,
-            Servers,
-            'app-server-details': ServerDetails,
-            'app-footer': Footer
-        }
-    }
+    
 </script>
 
 <style>
