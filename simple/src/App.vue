feefaccff9ab3ccf9877e1cf09f19c78fd13c181
jhyun94 <hyunjonathan@gmail.com>
2018-01-05 16:02:25 -0600

    dyanmic components

--- simple/src/App.vue
+++ simple/src/App.vue
@@ -1,8 +1,32 @@
 <template>
-   
+   <div>
+    <button @click="component = 'red'">Red Template</button>
+    <button @click="component = 'blue'">Blue Template</button>
+    <button @click="component = 'green'">Green Template</button>
+
+    <component :is="component"></component>
+   </div>
 </template>
 
 <script>
+  import blue from './components/blue'
+  import red from './components/red'
+  import green from './components/green'
+
+  export default {
+    components: {
+      blue,
+      red,
+      green
+    },
+
+    data(){
+      return {
+        component: 'red'
+      }
+    }
+  }
+
     
 </script>
 
