77e622ca3c8237861970daa2eed918631791091c
jhyun94 <hyunjonathan@gmail.com>
2018-01-06 14:22:36 -0600

    forms

--- simple/src/App.vue
+++ simple/src/App.vue
@@ -1,35 +1,84 @@
 <template>
-   <div>
-    <button @click="component = 'red'">Red Template</button>
-    <button @click="component = 'blue'">Blue Template</button>
-    <button @click="component = 'green'">Green Template</button>
+    <div class="container">
+        <form>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <!-- Exercise 1 -->
+                    <!-- Create a Signup Form where you retrieve the following Information -->
+                    <!-- Full Name (First Name + Last Name) -->
+                    <!-- Mail -->
+                    <!-- Password -->
+                    <!-- Store Data? Yes/No -->
 
-    <component :is="component"></component>
-   </div>
+                    <!-- Exercise 2 -->
+                    <!-- Only display the Form if it has NOT been submitted -->
+                    <!-- Display the Data Summary ONCE the Form HAS been submitted -->
+
+                    <!-- Exercise 3 -->
+                    <!-- Edit the Example from above and create a custom "Full Name" Control -->
+                    <!-- which still holds the First Name and Last Name Input Field -->
+                    <form v-if="!isSubmitted">
+                      <label>Full Name</label>
+                      <input type="text"
+                        v-model="userData.fullname">
+
+                      <label>Email</label>
+                      <input type="text"
+                        v-model="userData.email">
+
+                      <label>Password</label>
+                      <input type="password"
+                        v-model="userData.password">
+
+                      <label>Store Data</label>
+                      <input type="radio" v-model="storeData" value="yes">Yes
+                      <input type="radio" v-model="storeData" value="no">No
+
+                      <button type="submit" @click.prevent="submitted">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </form>
+        <hr>
+        <div class="row" v-if="isSubmitted">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4>Your Data</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p>Full Name: {{userData.fullname}}</p>
+                        <p>Mail: {{userData.email}}</p>
+                        <p>Password: {{userData.password}}</p>
+                        <p>Store in Database?: {{storeData}}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-  import blue from './components/blue'
-  import red from './components/red'
-  import green from './components/green'
+    export default {
+      data(){
+        return {
+          userData: {
+            fullname: '',
+            password: '',
+            email: ''
+          },
+          storeData: '',
+          isSubmitted: false
+        }
+      },
 
-  export default {
-    components: {
-      blue,
-      red,
-      green
-    },
-
-    data(){
-      return {
-        component: 'red'
+      methods: {
+        submitted(){
+          this.isSubmitted = true
+        }
       }
     }
-  }
-
-    
 </script>
 
 <style>
-
 </style>
