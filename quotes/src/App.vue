ed08399ff3984c2ebb37d215374e2c2376c5e67f
jhyun94 <hyunjonathan@gmail.com>
2018-01-06 11:02:14 -0600

    created newquotes, quotes component

---
+++ quotes/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h4>Quotes Added</h4>
+
+    <div>
+      <p>Info: Click on a quote to delete</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+
+</style>
